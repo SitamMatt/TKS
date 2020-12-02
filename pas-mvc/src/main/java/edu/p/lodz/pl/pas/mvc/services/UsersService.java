c678b5bf5f60008644381272dc1efb0d1591a788
Dominik Karski <dominik-karski@wp.pl>
2020-12-03 00:40:14 +0100

    Additional information of user's role in welcome.xhtml. LogoutBean fix.

--- pas-mvc/src/main/java/edu/p/lodz/pl/pas/mvc/services/UsersService.java
+++ pas-mvc/src/main/java/edu/p/lodz/pl/pas/mvc/services/UsersService.java
@@ -8,8 +8,10 @@ import edu.p.lodz.pl.pas.mvc.repositories.UsersRepository;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 import java.util.List;
 
+@Named
 @RequestScoped
 public class UsersService {
     @Inject
