e2e20fa5441ab7e92e19e6b4c4f2e8681d744f4e
SitamMatt <mateuszszewc@op.pl>
2020-12-15 13:33:26 +0100

    xx

--- pas-mvc/src/main/java/edu/p/lodz/pl/pas/mvc/controllers/UsersController.java
+++ pas-mvc/src/main/java/edu/p/lodz/pl/pas/mvc/controllers/UsersController.java
@@ -3,16 +3,41 @@ package edu.p.lodz.pl.pas.mvc.controllers;
 import edu.p.lodz.pl.pas.mvc.services.UsersService;
 import edu.p.lodz.pl.pas.mvc.services.dto.UserDto;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-@RequestScoped
+@ViewScoped
 @Named
-public class UsersController {
+public class UsersController implements Serializable {
+
+    public List<UserDto> getUsers() {
+        return users;
+    }
+
+    private List<UserDto> users;
+
+    @PostConstruct
+    public void init(){
+        this.users = usersService.getAllUsers();
+    }
+
+    public void loadUsers(){
+        if(searchQuery != null) {
+            UserDto user = usersService.find(searchQuery);
+            if(user.getId() == null) {
+                users =  new ArrayList<>();
+            }else{
+                users =  Collections.singletonList(user);
+            }
+        }
+    }
 
     @Inject
     private UsersService usersService;
@@ -39,16 +64,16 @@ public class UsersController {
         return "UserSummary";
     }
 
-    public List<UserDto> listUsers() throws CloneNotSupportedException {
-        if(searchQuery != null) {
-            UserDto user = usersService.find(searchQuery);
-            if(user.getId() == null) {
-                return new ArrayList<>();
-            }
-            return Collections.singletonList(user);
-        }
-        return usersService.getAllUsers();
-    }
+//    public List<UserDto> listUsers() throws CloneNotSupportedException {
+//        if(searchQuery != null) {
+//            UserDto user = usersService.find(searchQuery);
+//            if(user.getId() == null) {
+//                return new ArrayList<>();
+//            }
+//            return Collections.singletonList(user);
+//        }
+//        return usersService.getAllUsers();
+//    }
 
     public String search() {
         return "usersList.xhtml?faces-redirect=true&searchQuery=" + searchQuery;
