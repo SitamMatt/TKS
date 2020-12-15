4a70f616bbb9a3bfe2d7213d9b3aad4045fedd7d
SitamMatt <mateuszszewc@op.pl>
2020-12-15 14:35:08 +0100

    xxxxxxx

--- pas-mvc/src/main/java/edu/p/lodz/pl/pas/mvc/controllers/RentingController.java
+++ pas-mvc/src/main/java/edu/p/lodz/pl/pas/mvc/controllers/RentingController.java
@@ -3,6 +3,7 @@ package edu.p.lodz.pl.pas.mvc.controllers;
 import edu.p.lodz.pl.pas.mvc.model.exceptions.ObjectAlreadyStoredException;
 import edu.p.lodz.pl.pas.mvc.model.exceptions.RepositoryException;
 import edu.p.lodz.pl.pas.mvc.model.exceptions.ResourceNotAvailableException;
+import edu.p.lodz.pl.pas.mvc.model.exceptions.UserNotActiveException;
 import edu.p.lodz.pl.pas.mvc.services.EventsService;
 import edu.p.lodz.pl.pas.mvc.services.ResourcesService;
 import edu.p.lodz.pl.pas.mvc.services.dto.ResourceDto;
