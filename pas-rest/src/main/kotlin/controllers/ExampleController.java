c83820b94176cd417a789a3e3588bf3082365bc9
Mateusz Szewc <mszewc@lbpro.pl>
2020-12-27 14:55:08 +0100

    Example of basic rest features

--- pas-rest/src/main/kotlin/controllers/ExampleController.java
+++ pas-rest/src/main/kotlin/controllers/ExampleController.java
@@ -1,13 +1,19 @@
 package controllers;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import services.ExampleService;
 
 @Path("example")
 public class ExampleController {
+    @Inject
+    private ExampleService service;
+    
+    
     @GET
     public Response get(){
-        return Response.ok("Hello World").build();
+        return Response.ok().entity(service.getModel()).build();
     }
 }
