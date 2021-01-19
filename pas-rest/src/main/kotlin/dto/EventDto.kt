50ad6750ea11c72042b634fd65a76f959bff67f6
Mateusz Szewc <mszewc@lbpro.pl>
2021-01-19 01:48:19 +0100

    added EventsController tests

--- pas-rest/src/main/kotlin/dto/EventDto.kt
+++ pas-rest/src/main/kotlin/dto/EventDto.kt
@@ -1,10 +1,13 @@
 package dto
 
 import java.util.*
+import javax.json.bind.annotation.JsonbDateFormat
 
 class EventDto{
     var guid: UUID? = null
+    @JsonbDateFormat(value = "yyyy-MM-dd'T'HH:mm:ssXXX")
     var rentDate: Date? = null
+    @JsonbDateFormat(value = "yyyy-MM-dd'T'HH:mm:ssXXX")
     var returnDate: Date? = null
     var userId: UUID? = null
     var resourceId: UUID? = null
