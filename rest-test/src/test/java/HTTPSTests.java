77a772f9e935487abe2526d058d1abd085e13bea
Dominik Karski <dominik-karski@wp.pl>
2021-01-19 13:26:46 +0100

    https test

---
+++ rest-test/src/test/java/HTTPSTests.java
@@ -0,0 +1,35 @@
+import helpers.Helpers;
+import io.restassured.RestAssured;
+import org.json.JSONException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class HTTPSTests {
+    @Before
+    public void setup() {
+        RestAssured.baseURI = "https://localhost:8181/pas-rest-1.0-SNAPSHOT";
+    }
+
+    public static final String HttpUri = "http://localhost:8080/pas-rest-1.0-SNAPSHOT";
+
+    @Test
+    public void httpRequestsShouldNotBeAllowed() throws JSONException {
+        String token = Helpers.extractToken(Helpers.auth("admin", "admin0"));
+        try {
+            var res = given()
+                    .contentType("application/json")
+                    .headers("Authorization", "Bearer " + token)
+                    .when().get(HttpUri + "/api/users/me");
+            res.then()
+                    .contentType("application/json")
+                    .statusCode(302);
+            Assert.fail();
+        } catch (Exception ignored) {
+
+        }
+
+    }
+}
