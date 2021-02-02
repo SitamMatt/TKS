07aed4ce1d50f262a310bc1a8820d8062ad0aa76
Mateusz Szewc <mszewc@lbpro.pl>
2021-02-02 15:00:25 +0100

    final cut

--- spa-svelte/src/App.svelte
+++ spa-svelte/src/App.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
-    import { Router, Link, Route } from "svelte-navigator";
+    import { Router, Link, Route, navigate } from "svelte-navigator";
     import Login from "./pages/Login.svelte";
     import DashboardView from "./pages/DashboardView.svelte";
-    import { tokenRefreshed } from "./stores/auth-store";
+    import { getToken, tokenRefreshed } from "./stores/auth-store";
     import { Notifications, notifier } from "smelte/src/components/Snackbar";
     import UserFormView from "./pages/UserFormView.svelte";
+    import { onMount } from "svelte";
 
     tokenRefreshed.subscribe((val) => {
         console.log(val);
         if (val) notifier.notify("Token has been refreshed");
+    });
+
+    onMount(async () => {
+        let token = await getToken();
+        if (token !== undefined) {
+            navigate("/dashboard");
+        }
     });
 </script>
 
--- spa-svelte/src/stores/auth-store.ts
+++ spa-svelte/src/stores/auth-store.ts
@@ -10,34 +10,43 @@ export const tokenRefreshed = writable<boolean>(false)
 export const acquireToken = async (login: string, password: string) => {
     let token = await requestToken(login, password);
     tokenStore.set(token)
+    localStorage.setItem("token", JSON.stringify(token))
 }
 
 
 
 export const getToken = async () => {
     let tokenObj = get(tokenStore);
-    if(tokenObj == null){
+    console.log("Token: "+tokenObj)
+    if(tokenObj === null){
         // force to acquire token
-        console.log("Token is null");
-        return;
+        tokenObj = JSON.parse(localStorage.getItem("token")) as Token
+            console.log("Token in store: "+tokenObj)
+
+        if(tokenObj == null){
+            console.log("Token is null");
+            return;
+        }
     }
     // check if token expired
     let date = new Date();
+    let now = new Date()
     date.setSeconds(date.getSeconds() + 30)
-    let tokenExpirationDate = new Date(tokenObj.expires)
+    let tokenRefreshExpirationDate = new Date(tokenObj.expires)
     console.log(date)
-    console.log(tokenExpirationDate)
-    if(tokenExpirationDate < date){
+    console.log(tokenRefreshExpirationDate)
+    console.log(now)
+    if(tokenRefreshExpirationDate < now){
+        console.log("token expired")
+        return;
+    }
+    else if(tokenRefreshExpirationDate < date){
         console.log("refreshing token")
         tokenRefreshed.set(true)
         let token = await requestTokenRefresh(tokenObj.token)
         tokenStore.set(token)
         tokenObj = get(tokenStore)
         tokenRefreshed.set(false)
-
-        // setTimeout(() => {
-        //     tokenRefreshed.set(false)
-        // }, 2000);
     }
     return tokenObj.token;
 }
