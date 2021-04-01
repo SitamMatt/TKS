27eaa8e0501ce940a083cefd6d93edf608fb0251
Mateusz Szewc <mszewc@lbpro.pl>
2021-04-01 12:49:50 +0200

    Producers added

--- tks/RestAdapters/src/main/kotlin/Producer.java
+++ tks/RestAdapters/src/main/kotlin/Producer.java
@@ -1,13 +1,36 @@
+import adapters.UserRepositoryAdapter;
+import data.UserEntity;
+import drivenports.UserQueryPort;
+import drivenports.UserSavePort;
+import mappers.UserMapper;
+import repositories.RepositoryBase;
 import services.UserService;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import java.util.ArrayList;
 
 @ApplicationScoped
 public class Producer {
 
     @Produces
-    public UserService produceUserService(){
-        return new UserService();
+    public UserService produceUserService(UserSavePort userSavePort, UserQueryPort userQueryPort){
+        return new UserService(userSavePort, userQueryPort);
+    }
+
+    @Produces
+    public UserRepositoryAdapter produceUserSavePort(RepositoryBase<UserEntity> repository, UserMapper mapper){
+        return new UserRepositoryAdapter(repository, mapper);
+    }
+
+    @Produces
+    public RepositoryBase<UserEntity> produceUserRepository(){
+        var list = new ArrayList<UserEntity>();
+        return new RepositoryBase<>(list);
+    }
+
+    @Produces
+    public UserMapper produceUserMapper(){
+        return UserMapper.INSTANCE;
     }
 }
