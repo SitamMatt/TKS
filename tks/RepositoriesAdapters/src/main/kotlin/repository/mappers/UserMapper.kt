14be9ad47ea2792fb673eb35e76ecc0c62a74ab2
Mateusz Szewc <mszewc@lbpro.pl>
2021-04-17 22:57:23 +0200

    missing files

---
+++ tks/RepositoriesAdapters/src/main/kotlin/repository/mappers/UserMapper.kt
@@ -0,0 +1,19 @@
+package repository.mappers
+
+import common.mappers.EmailMapper
+import repository.data.UserEntity
+import org.mapstruct.MappingTarget
+import domain.model.User
+import org.mapstruct.Mapper
+import org.mapstruct.factory.Mappers
+
+@Mapper(uses =[EmailMapper::class])
+interface UserMapper {
+    fun mapEntityToDomainObject(entity: UserEntity?): User?
+    fun mapDomainObjectToEntity(user: User?): UserEntity?
+    fun mapDomainObjectToEntity(user: User?, @MappingTarget entity: UserEntity?)
+
+    companion object {
+        val INSTANCE: UserMapper = Mappers.getMapper(UserMapper::class.java)
+    }
+}
