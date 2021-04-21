f89eaf966003399712764549257f1f82138d596c
224464 Zabroń Mateusz <matzab37@gmail.com>
2021-04-21 22:41:12 +0200

    RentResourceTest improved

--- tks/RestAdapters/src/test/kotlin/rest/api/controllers/RentResourceTest.kt
+++ tks/RestAdapters/src/test/kotlin/rest/api/controllers/RentResourceTest.kt
@@ -10,10 +10,10 @@ class RentResourceTest {
     fun `Given valid email and accessing number rent should success`() {
         RestAssured.given()
                 .port(8080)
-                .baseUri("http://localhost/tks/api/library/item/EEEE-154/rent")
+                .baseUri("http://localhost/tks/api/library/item/EEEE-154/rent/by/mszewc@edu.pl")
                 .post()
                 .then()
-                .statusCode(200)
+                .statusCode(201)
     }
 
     @Test
@@ -22,7 +22,7 @@ class RentResourceTest {
 
         val response = RestAssured.given()
                 .port(8080)
-                .baseUri("http://localhost/tks/api/library/item/EEEE-254/rent")
+                .baseUri("http://localhost/tks/api/library/item/EEEE-254/rent/by/mszewc@edu.pl")
                 .post()
 
         response.then().statusCode(409)
@@ -30,18 +30,12 @@ class RentResourceTest {
         Assertions.assertEquals(expected, body)
     }
 
-//    @Test
-//    fun `Given not valid email rent should return NOT FOUND`(){
-//        val expected = ErrorDto("User not found", 1)
-//    }
-
     @Test
-    fun `Given not valid accession number rent should return NOT FOUND`(){
-        val expected = ErrorDto("Resource not found", 2)
-
+    fun `Given not valid email rent should return NOT FOUND`(){
+        val expected = ErrorDto("User not found", 1)
         val response = RestAssured.given()
                 .port(8080)
-                .baseUri("http://localhost/tks/api/library/item/EEEE-214/rent")
+                .baseUri("http://localhost/tks/api/library/item/EEEE-214/rent/by/michal@edu.pl")
                 .post()
 
         response.then().statusCode(404)
@@ -49,30 +43,76 @@ class RentResourceTest {
         Assertions.assertEquals(expected, body)
     }
 
-//    @Test
-//    fun `Given not active user email rent should return INACTIVE USER`(){
-//        val expected = ErrorDto("User is inactive", 12)
-//    }
+    @Test
+    fun `Given not valid accession number rent should return NOT FOUND`(){
+        val expected = ErrorDto("Resource not found", 2)
+
+        val response = RestAssured.given()
+                .port(8080)
+                .baseUri("http://localhost/tks/api/library/item/EEEE-214/rent/by/mszewc@edu.pl")
+                .post()
+
+        response.then().statusCode(404)
+        val body = response.body.`as`<ErrorDto>(ErrorDto::class.java)
+        Assertions.assertEquals(expected, body)
+    }
+
+    @Test
+    fun `Given not active user email rent should return INACTIVE USER`(){
+        val expected = ErrorDto("User is inactive", 12)
+
+        val response = RestAssured.given()
+                .port(8080)
+                .baseUri("http://localhost/tks/api/library/item/EEEE-254/rent/by/marcin@edu.pl")
+                .post()
+
+        response.then().statusCode(400)
+        val body = response.body.`as`<ErrorDto>(ErrorDto::class.java)
+        Assertions.assertEquals(expected, body)
+    }
 
     @Test
     fun `Given valid email and accessing number rent delete should success`(){
         RestAssured.given()
                 .port(8080)
-                .baseUri("http://localhost/tks/api/library/item/EEEE-254/rent")
+                .baseUri("http://localhost/tks/api/library/item/EEEE-303/rent/by/mszewc@edu.pl")
+                .post()
+                .then()
+                .statusCode(201)
+
+        RestAssured.given()
+                .port(8080)
+                .baseUri("http://localhost/tks/api/library/item/EEEE-303/rent/by/mszewc@edu.pl")
                 .delete()
                 .then()
                 .statusCode(200)
     }
 
-//    @Test
-//    fun `Given not valid email rent delete should return NOT FOUND`(){
-//        val expected = ErrorDto("User not found", 1)
-//    }
+    @Test
+    fun `Given not valid email rent delete should return NOT FOUND`(){
+        val expected = ErrorDto("User not found", 1)
+        val response = RestAssured.given()
+                .port(8080)
+                .baseUri("http://localhost/tks/api/library/item/EEEE-214/rent/by/michal@edu.pl")
+                .delete()
 
-//    @Test
-//    fun `Given wrong user email rent delete should return INVALID USER`(){
-//        val expected = ErrorDto("Rent does not exist", 3)
-//    }
+        response.then().statusCode(404)
+        val body = response.body.`as`<ErrorDto>(ErrorDto::class.java)
+        Assertions.assertEquals(expected, body)
+    }
+
+    @Test
+    fun `Given wrong user email rent delete should return INVALID USER`(){
+        val expected = ErrorDto("Rent does not exist", 3)
+        val response = RestAssured.given()
+                .port(8080)
+                .baseUri("http://localhost/tks/api/library/item/EEEE-154/rent/by/marcin@edu.pl")
+                .delete()
+
+        response.then().statusCode(400)
+        val body = response.body.`as`<ErrorDto>(ErrorDto::class.java)
+        Assertions.assertEquals(expected, body)
+    }
 
     @Test
     fun `Given not valid accession number rent delete should return NOT FOUND`(){
@@ -81,7 +121,7 @@ class RentResourceTest {
 
         val response = RestAssured.given()
                 .port(8080)
-                .baseUri("http://localhost/tks/api/library/item/EEEE-214/rent")
+                .baseUri("http://localhost/tks/api/library/item/EEEE-214/rent/by/mszewc@edu.pl")
                 .delete()
 
         response.then().statusCode(404)
@@ -96,10 +136,10 @@ class RentResourceTest {
 
         val response = RestAssured.given()
                 .port(8080)
-                .baseUri("http://localhost/tks/api/library/item/EEEE-303/rent")
+                .baseUri("http://localhost/tks/api/library/item/EEEE-303/rent/by/mszewc@edu.pl")
                 .delete()
 
-        response.then().statusCode(409)
+        response.then().statusCode(400)
         val body = response.body.`as`<ErrorDto>(ErrorDto::class.java)
         Assertions.assertEquals(expected, body)
     }
