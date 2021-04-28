04e8c253fc128ad0ee873eb84cd25f5445f1815a
224464 Zabroń Mateusz <matzab37@gmail.com>
2021-04-28 16:16:29 +0200

    UserResource POST testing.

--- tks/Microservices/user-service/src/test/kotlin/UserResourceTest.kt
+++ tks/Microservices/user-service/src/test/kotlin/UserResourceTest.kt
@@ -53,65 +53,65 @@ class UserResourceTest {
         Assertions.assertEquals(expected, body)
     }
 
-//    @Test
-//    fun `Given valid user, post should create new user resource`() {
-//        val model = UserDto("matzab@edu.pl", "password", true, "CLIENT")
-//        val expected = UserDto("matzab@edu.pl", null, true, "CLIENT")
-//
-//        RestAssured.given()
-//            .port(8080)
-//            .baseUri("http://localhost/user-service/app/user")
-//            .contentType(ContentType.JSON)
-//            .body(model)
-//            .post()
-//            .then()
-//            .statusCode(201)
-//            .header("Location", StringContains.containsString("/user/matzab@edu.pl"))
-//            .assertThat()
-//            .body("$", Matchers.not(Matchers.hasKey("password")))
-//            .body("active", Is.`is`(true))
-//
-//        val response = RestAssured.given()
-//            .port(8080)
-//            .baseUri("http://localhost/user-service/app/user")
-//            .contentType(ContentType.JSON)
-//            .get("/matzab@edu.pl")
-//
-//        response.then().statusCode(200)
-//        val body = response.body.`as`<UserDto>(UserDto::class.java)
-//        Assertions.assertEquals(expected, body)
-//    }
+    @Test
+    fun `Given valid user, post should create new user resource`() {
+        val model = UserDto("matzab@edu.pl", "password", true, "CLIENT")
+        val expected = UserDto("matzab@edu.pl", null, true, "CLIENT")
 
-//    @Test
-//    fun `Given user with duplicated email, post should return CONFLICT`() {
-//        val model1 = UserDto("mati@edu.pl", "password", true, "CLIENT")
-//        val model2 = UserDto("mati@edu.pl", "password", true, "CLIENT")
-//        val expected = ErrorDto("Email is taken", 13)
-//
-//        RestAssured.given()
-//            .port(8080)
-//            .baseUri("http://localhost/user-service/app/user")
-//            .contentType(ContentType.JSON)
-//            .body(model1)
-//            .post()
-//            .then()
-//            .statusCode(201)
-//            .header("Location", StringContains.containsString("/user/mati@edu.pl"))
-//            .assertThat()
-//            .body("$", Matchers.not(Matchers.hasKey("password")))
-//            .body("active", Is.`is`(true))
-//
-//        val response = RestAssured.given()
-//            .port(8080)
-//            .baseUri("http://localhost/user-service/app/user")
-//            .contentType(ContentType.JSON)
-//            .body(model2)
-//            .post()
-//
-//        response.then().statusCode(409)
-//        val body = response.body.`as`<ErrorDto>(ErrorDto::class.java)
-//        Assertions.assertEquals(expected, body)
-//    }
+        RestAssured.given()
+            .port(8080)
+            .baseUri("http://localhost/user-service/app/user")
+            .contentType(ContentType.JSON)
+            .body(model)
+            .post()
+            .then()
+            .statusCode(201)
+            .header("Location", StringContains.containsString("/user/matzab@edu.pl"))
+            .assertThat()
+            .body("$", Matchers.not(Matchers.hasKey("password")))
+            .body("active", Is.`is`(true))
+
+        val response = RestAssured.given()
+            .port(8080)
+            .baseUri("http://localhost/user-service/app/user")
+            .contentType(ContentType.JSON)
+            .get("/matzab@edu.pl")
+
+        response.then().statusCode(200)
+        val body = response.body.`as`<UserDto>(UserDto::class.java)
+        Assertions.assertEquals(expected, body)
+    }
+
+    @Test
+    fun `Given user with duplicated email, post should return CONFLICT`() {
+        val model1 = UserDto("mati@edu.pl", "password", true, "CLIENT")
+        val model2 = UserDto("mati@edu.pl", "password", true, "CLIENT")
+        val expected = ErrorDto("Email is taken", 13)
+
+        RestAssured.given()
+            .port(8080)
+            .baseUri("http://localhost/user-service/app/user")
+            .contentType(ContentType.JSON)
+            .body(model1)
+            .post()
+            .then()
+            .statusCode(201)
+            .header("Location", StringContains.containsString("/user/mati@edu.pl"))
+            .assertThat()
+            .body("$", Matchers.not(Matchers.hasKey("password")))
+            .body("active", Is.`is`(true))
+
+        val response = RestAssured.given()
+            .port(8080)
+            .baseUri("http://localhost/user-service/app/user")
+            .contentType(ContentType.JSON)
+            .body(model2)
+            .post()
+
+        response.then().statusCode(409)
+        val body = response.body.`as`<ErrorDto>(ErrorDto::class.java)
+        Assertions.assertEquals(expected, body)
+    }
 
 
 }
