4194288cfd59a3b30f099cf910c10d00bf4323db
matt <mateuszszewc@op.pl>
2021-04-28 15:55:45 +0200

    uncommented method in UserResource.kt

--- tks/Microservices/user-service/src/main/kotlin/edu/p/lodz/pl/service/users/UserResource.kt
+++ tks/Microservices/user-service/src/main/kotlin/edu/p/lodz/pl/service/users/UserResource.kt
@@ -1,19 +1,22 @@
 package edu.p.lodz.pl.service.users
 
 import common.error.badRequest
+import common.error.conflict
 import common.error.notFound
+import domain.exceptions.DuplicatedEmailException
 import domain.exceptions.TypeValidationFailedException
 import domain.exceptions.UserNotFoundException
 import edu.p.lodz.pl.service.users.adapters.UserResourceAdapter
+import edu.p.lodz.pl.service.users.dto.UserDto
 import edu.p.lodz.pl.service.users.mappers.UserMapperDto
 import org.eclipse.microprofile.openapi.annotations.Operation
+import java.net.URI
 import javax.enterprise.context.RequestScoped
 import javax.inject.Inject
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.PathParam
-import javax.ws.rs.Produces
+import javax.ws.rs.*
+import javax.ws.rs.core.Context
 import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriInfo
 
 @Path("user")
 open class UserResource {
@@ -23,13 +26,6 @@ open class UserResource {
 
     @Inject
     private var mapper: UserMapperDto? = null
-
-    @GET
-    open fun get(): Response? {
-        val e = "aaaa";
-        val r = e + "555"
-        return Response.ok(r).build()
-    }
 
     @GET
     @Path("{id}")
@@ -47,19 +43,19 @@ open class UserResource {
     } catch (e: TypeValidationFailedException) {
         e.error.badRequest()
     }
-//
-//    @POST
-//    @Produces("application/json")
-//    @Operation(
-//        operationId = "Register new user",
-//        description = "Registers new user from given repository.data",
-//        summary = "Register new user"
-//    )
-//    fun post(dto: UserDto?, @Context context: UriInfo): Response = try {
-//        adapter?.registerCommand(dto)
-//        val resourceLink: URI = context.absolutePathBuilder.path(dto!!.email).build()
-//        Response.created(resourceLink).entity(dto).build()
-//    } catch (e: DuplicatedEmailException) {
-//        e.error.conflict()
-//    }
+
+    @POST
+    @Produces("application/json")
+    @Operation(
+        operationId = "Register new user",
+        description = "Registers new user from given repository.data",
+        summary = "Register new user"
+    )
+    open fun post(dto: UserDto?, @Context context: UriInfo): Response = try {
+        adapter?.registerCommand(dto)
+        val resourceLink: URI = context.absolutePathBuilder.path(dto!!.email).build()
+        Response.created(resourceLink).entity(dto).build()
+    } catch (e: DuplicatedEmailException) {
+        e.error.conflict()
+    }
 }
