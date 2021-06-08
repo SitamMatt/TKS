491c2c5125578c393df20cd08938ed74fd13fd0d
Mateusz Szewc <mszewc@lbpro.pl>
2021-06-08 11:53:17 +0200

    micronaut testcontainers setup tests

---
+++ microservices/microservices-library/src/main/kotlin/microservices/library/StartupListener.kt
@@ -0,0 +1,17 @@
+//package microservices.library
+//
+//import io.micronaut.context.event.ApplicationEventListener
+//import io.micronaut.runtime.server.event.ServerStartupEvent
+//import javax.inject.Inject
+//import javax.inject.Singleton
+//
+//@Singleton
+//class StartupListener : ApplicationEventListener<ServerStartupEvent> {
+//
+//    @Inject
+//    private lateinit var
+//
+//    override fun onApplicationEvent(event: ServerStartupEvent?) {
+//
+//    }
+//}
--- microservices/microservices-library/src/test/kotlin/microservices/library/MicroservicesLibraryTest.kt
+++ microservices/microservices-library/src/test/kotlin/microservices/library/MicroservicesLibraryTest.kt
@@ -1,13 +1,71 @@
 package microservices.library
 
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.env.PropertySource
 import io.micronaut.runtime.EmbeddedApplication
-import io.micronaut.test.extensions.kotest.annotation.MicronautTest
-import io.kotest.core.spec.style.StringSpec
+import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.restassured.RestAssured
+import io.restassured.RestAssured.config
+import io.restassured.config.LogConfig
+import io.restassured.filter.log.ResponseLoggingFilter
+import io.restassured.module.kotlin.extensions.Extract
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
 
-@MicronautTest
-class MicroservicesLibraryTest(private val application: EmbeddedApplication<*>) : StringSpec({
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class DemoTest {
 
-    "test the server is running" {
-        assert(application.isRunning)
+
+    class KGenericContainer(imageName: String) : PostgreSQLContainer<KGenericContainer>(imageName)
+
+    val application: EmbeddedApplication<*>
+
+    init {
+        db.start()
+        application =
+            ApplicationContext.run(
+                EmbeddedServer::class.java,
+                PropertySource.of("test", mapOf("datasources.default.url" to db.jdbcUrl))
+            )
+        RestAssured.baseURI = application.uri.toString()
+        RestAssured.port = application.port
+        RestAssured.config = config().logConfig(LogConfig.logConfig().enablePrettyPrinting(true))
     }
-})
+
+    companion object {
+        @Container
+        @JvmField
+        val db: KGenericContainer = KGenericContainer("postgres:13")
+            .withUsername("postgres")
+            .withDatabaseName("libraryDB")
+            .withPassword("postgres")
+            .withExposedPorts(5432)
+    }
+
+    @Test
+    fun testItWorks() {
+        val rentId: String = Given {
+            filter(ResponseLoggingFilter.logResponseTo(System.out))
+            pathParam("id", "EEEE-254")
+        } When {
+            get("library/{id}")
+        } Then {
+            statusCode(200)
+        } Extract {
+            path("accessionNumber")
+        }
+
+        rentId shouldNotBe null
+        rentId shouldBe "EEEE-254"
+    }
+
+}
