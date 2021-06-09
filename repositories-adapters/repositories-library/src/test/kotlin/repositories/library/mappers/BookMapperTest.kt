47e8f835eb56bf4dd15d355b62afc1d89f08c978
224464 Zabroń Mateusz <matzab37@gmail.com>
2021-06-09 17:32:43 +0200

    BookMapperTest magazine mapping added

--- repositories-adapters/repositories-library/src/test/kotlin/repositories/library/mappers/BookMapperTest.kt
+++ repositories-adapters/repositories-library/src/test/kotlin/repositories/library/mappers/BookMapperTest.kt
@@ -2,11 +2,13 @@ package repositories.library.mappers
 
 import core.domain.common.valueobjects.AccessionNumber
 import core.domain.resource.Book
+import core.domain.resource.Magazine
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class BookMapperTest {
     private val book = Book(AccessionNumber("EEEE-221"), "Pan Tadeusz", false, "Adam Mickiewicz")
+    private val magazine = Magazine(AccessionNumber("MMMM-102"), "Super Gigant", false, "Nowa Era")
 
     @Test
     fun bookToEntityTest(){
@@ -27,5 +29,14 @@ class BookMapperTest {
             domain.locked shouldBe book.locked
             domain.title shouldBe book.title
         }
+    }
+
+    @Test
+    fun magazineToEntityTest(){
+        val dto = magazine.toEntity()
+        dto.accessionNumber shouldBe magazine.accessionNumber?.value
+        dto.title shouldBe magazine.title
+        dto.locked shouldBe magazine.locked
+        dto.id shouldBe 0
     }
 }
