17495f43fb2ba20a33bd56ade37e327c965abb4b
224464 Zabroń Mateusz <matzab37@gmail.com>
2021-06-09 17:52:37 +0200

    testing product mapper

--- repositories-adapters/repositories-rental/src/test/kotlin/repositories/rental/mappers/ProductMapperTest.kt
+++ repositories-adapters/repositories-rental/src/test/kotlin/repositories/rental/mappers/ProductMapperTest.kt
@@ -1,4 +1,24 @@
 package repositories.rental.mappers
 
+import core.domain.common.valueobjects.AccessionNumber
+import core.domain.rent.Product
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
 class ProductMapperTest {
+    private val product = Product(AccessionNumber("EEEE-121"))
+    private val dto = product.toEntity()
+    private val domain = dto.toDomain()
+
+
+    @Test
+    fun productToEntityTest(){
+        dto.id shouldBe 0
+        dto.accessionNumber shouldBe product.accessionNumber?.value
+    }
+
+    @Test
+    fun productEntityToDomainTest(){
+        domain.accessionNumber shouldBe product.accessionNumber
+    }
 }
