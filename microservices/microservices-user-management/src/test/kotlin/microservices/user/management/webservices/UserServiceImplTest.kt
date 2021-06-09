340f68bb0188b6ae573f3b49338315513ffd055a
224464 Zabroń Mateusz <matzab37@gmail.com>
2021-06-09 13:09:57 +0200

    Change user state test added.

--- microservices/microservices-user-management/src/test/kotlin/microservices/user/management/webservices/UserServiceImplTest.kt
+++ microservices/microservices-user-management/src/test/kotlin/microservices/user/management/webservices/UserServiceImplTest.kt
@@ -68,4 +68,34 @@ internal class UserServiceImplTest{
         println(user.password)
     }
 
+    @Test
+    fun changeUserStateTest(){
+        val consumer: KafkaConsumer<String, String> = KafkaConsumer(
+                ImmutableMap.of(
+                        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaResource.x,
+                        ConsumerConfig.GROUP_ID_CONFIG, "tc-" + UUID.randomUUID(),
+                        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"
+                ) as Map<String, Any>?,
+                StringDeserializer(),
+                StringDeserializer()
+        )
+        consumer.subscribe(Collections.singletonList("clients"))
+        val service  = UserServiceService(URL("http://localhost:9090/soap/userservice?wsdl"))
+        val port = service.userServicePort
+        val dto = UserDto()
+        dto.email = "adamKowal@edu.pl"
+        dto.isActive = true
+        dto.password = "####"
+        dto.role = "ADMIN"
+        port.registerUser(dto)
+        Thread.sleep(5000)
+        val records = consumer.poll(Duration.ofMillis(2000))
+        records.count() shouldBeGreaterThan 0
+
+        val beforeChange = port.getUser("adamKowal@edu.pl")
+        port.changeUserState(beforeChange.email, !beforeChange.isActive)
+        val afterChange = port.getUser(beforeChange.email)
+        afterChange.isActive shouldBe !beforeChange.isActive
+    }
+
 }
